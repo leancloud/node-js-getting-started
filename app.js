4e1212c598fe7712799dd0e1ab5253c499b058d6
jysperm <jysperm@gmail.com>
2017-11-03 14:45:51 +0800

    :tada: GraphQL getting started

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
+var leancloudGraphQL = require('leancloud-graphql').express;
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
@@ -31,6 +32,8 @@ app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/graphql', leancloudGraphQL());
 
 app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() });
